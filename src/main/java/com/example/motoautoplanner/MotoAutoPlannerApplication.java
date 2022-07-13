56997b7c72dd3a5fd976202ea0f04eb2da55fedb
vladislav_domaniewski <vladislav_buko@mail.ru>
2022-07-13 23:07:21 +0300

    Uploaded start project

---
+++ src/main/java/com/example/motoautoplanner/MotoAutoPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.motoautoplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoAutoPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotoAutoPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/motoautoplanner/MotoAutoPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motoautoplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoAutoPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
