2ccceb7a4e2e17cebd5afce5128a7c3dd2e5aa88
vladislav_domaniewski <vladislav_buko@mail.ru>
2022-07-14 23:37:01 +0300

    Added new dependency. An error remains, the bot is not running

---
+++ src/main/java/com/example/motoautoplanner/config/ConfigForBot.java
@@ -0,0 +1,43 @@
+package com.example.motoautoplanner.config;
+
+import com.example.motoautoplanner.ApplicationMain;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+
+/**
+ * Created by Vladislav Domaniewski
+ */
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "telegrambot")
+public class ConfigForBot {
+
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
+
+    private DefaultBotOptions.ProxyType proxyType;
+    private String proxyHost;
+    private int proxyPort;
+
+    @Bean
+    public ApplicationMain MySuperTelegramBot() {
+        DefaultBotOptions options = ApiContext
+                .getInstance(DefaultBotOptions.class);
+
+        options.setProxyHost(proxyHost);
+        options.setProxyPort(proxyPort);
+        options.setProxyType(proxyType);
+
+        ApplicationMain mySuperTelegramBot = new ApplicationMain(options);
+        mySuperTelegramBot.setBotUserName(botUserName);
+        mySuperTelegramBot.setBotToken(botToken);
+        mySuperTelegramBot.setWebHookPath(webHookPath);
+
+        return mySuperTelegramBot;
+    }
+}
